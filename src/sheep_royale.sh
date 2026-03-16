63c414466e0bf97163b2db47798c555c361d9cdb
huok <77536370+vraestoren@users.noreply.github.com>
2026-03-16 14:41:27 +0300

    Refactor API calls to use _get function

--- src/sheep_royale.sh
+++ src/sheep_royale.sh
@@ -1,68 +1,49 @@
 #!/bin/bash
 
+params=null
 api="https://pitergo.website:30000"
-sign=null
-vk_user_id=null
-vk_ts=null
-vk_ref=null
 user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36"
 
+function _get() {
+    curl --request GET \
+        --url "$api/$1" \
+        --user-agent "$user_agent" \
+        --header "content-type: application/json" \
+        --header "authorization: $params"
+}
+
+# 1 - sign: (string): <sign>
+# 2 - vk_user_id: (integer): <vk_user_id>
+# 3 - vk_ts: (integer): <vk_ts>
+# 4 - vk_ref: (string): <vk_ref>
+# 5 - access_token_settings: (string): <access_token_settings - default: >
+# 6 - are_notifications_enabled: (integer): <are_notifications_enabled - default: 0>
+# 7 - is_app_user: (integer): <is_app_user - default: 0>
+# 8 - is_favorite: (integer): <is_favorite - default: 0>
+# 9 - language: (string): <language - default: ru>
+# 10 - platform: (string): <platform - default: desktop_web>
 function authenticate() {
-	# 1 - sign: (string): <sign>
-	# 2 - vk_user_id: (integer): <vk_user_id>
-	# 3 - vk_ts: (integer): <vk_ts>
-	# 4 - vk_ref: (string): <vk_ref>
-	# 5 - access_token_settings: (string): <access_token_settings - default: >
-	# 6 - are_notifications_enabled: (integer): <are_notifications_enabled: default: 0>
-	# 7 - is_app_user: (integer): <is_app_user - default: 0>
-	# 8 - is_favorite: (integer): <is_favorite - default: 0>
-	# 9 - language: (string): <language - default: ru>
-	# 10 - platform: (string): <platform - default: desktop_web>
-	sign=$1
-	vk_user_id=$2
-	vk_ts=$3
-	vk_ref=$4
-	params="vk_access_token_settings=${5:-}&vk_app_id=51491054&vk_are_notifications_enabled=${6:-0}&vk_is_app_user=${7:-0}&vk_is_favorite=${8:-0}&vk_language=${9:-ru}&vk_platform=${10:-desktop_web}&vk_ref=$vk_ref&vk_ts=$vk_ts&vk_user_id=$vk_user_id&sign=$sign"
-	echo $params
+    params="vk_access_token_settings=${5:-}&vk_app_id=51491054&vk_are_notifications_enabled=${6:-0}&vk_is_app_user=${7:-0}&vk_is_favorite=${8:-0}&vk_language=${9:-ru}&vk_platform=${10:-desktop_web}&vk_ref=$4&vk_ts=$3&vk_user_id=$2&sign=$1"
+    echo "$params"
 }
 
 function get_account_info() {
-	curl --request GET \
-		--url "$api/user/get" \
-		--user-agent "$user_agent" \
-		--header "content-type: application/json" \
-		--header "authorization: $params"
+    _get "user/get"
 }
 
 function get_online() {
-	curl --request GET \
-		--url "$api/user/online" \
-		--user-agent "$user_agent" \
-		--header "content-type: application/json" \
-		--header "authorization: $params"
-}
-
-function get_bonus() {
-	# 1 - method_name: (string): <method_name>
-	curl --request GET \
-		--url "$api/user/Bonuses?methodName=$1" \
-		--user-agent "$user_agent" \
-		--header "content-type: application/json" \
-		--header "authorization: $params"
+    _get "user/online"
 }
 
 function get_season() {
-	curl --request GET \
-		--url "$api/user/season" \
-		--user-agent "$user_agent" \
-		--header "content-type: application/json" \
-		--header "authorization: $params"
+    _get "user/season"
 }
 
 function get_rating() {
-	curl --request GET \
-		--url "$api/user/rating" \
-		--user-agent "$user_agent" \
-		--header "content-type: application/json" \
-		--header "authorization: $params"
+    _get "user/rating"
+}
+
+# 1 - method_name: (string): <method_name>
+function get_bonus() {
+    _get "user/Bonuses?methodName=$1"
 }
